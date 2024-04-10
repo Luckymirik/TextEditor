f71d5ecc30051adf7833dc14f463f14b5b4ed794
Miroslav Tsadkin <Luckymirik@mail.ru>
2024-04-10 12:18:48 +0300

    Pre-Finish

---
+++ src/Editor.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Editor extends JFrame{
+    private JButton button1;
+
+    private JPanel panel;
+    private JButton button3;
+    private JButton button2;
+    private JTextPane открытФайлTextPane;
+}
